afe15999cccb43e704042ce74ebec5bf6b228290
Anjan Momi <anjan@momi.ca>
2018-12-28 19:55:25 -0800

    test: ignore entire common folder and fix if-statement > 0

--- common/travis/checkFileName.sh
+++ common/travis/checkFileName.sh
@@ -8,14 +8,12 @@ wronglyNamedFiles=$(find . -type f |
     grep -v '^./.git/' |
     grep -v './README.md' |
     grep -v './.travis.yml' |
-    grep -v './common/travis/checkFileName.sh' |
+    grep -v '^./common/' |
     # print file names that do not match proper naming scheme
     grep -vE '^.\/[A-Z]+ [0-9]+\/(Final|Midterm|Homework)\/(Summer|Fall|Spring)-[0-9]{4}[^/]+$'
     )
 
-numberOfWronglyNamedFiles=$(echo $wronglyNamedFiles | wc -l)
-
-if [ numberOfWronglyNamedFiles != "0" ]
+if [ ! -z "$wronglyNamedFiles" ]
 then
    echo "The following files are not named with the proper naming convention and/or not placed in the proper folder hierarchy:"
    echo $wronglyNamedFiles
